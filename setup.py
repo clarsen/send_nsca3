4dccd700a4c280752dfaeedcb27f1c2e5eeaa0c5
Jeffrey Clark <jclark@nmi.com>
2021-08-30 21:52:17 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="send_nsca3",
-    version="0.1.5.5",
+    version="0.1.6.0",
     author="gitmstoute",
     author_email="",
     url="https://github.com/gitmstoute/send_nsca3",
