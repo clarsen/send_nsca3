c5e3994a78b8d97e6fb328d44e03775c1577ea3e
Case Larsen <CTLarsen@lbl.gov>
2022-02-28 19:06:52 -0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="send_nsca3",
-    version="0.1.6.0",
+    version="0.1.7.0-clarsen",
     author="gitmstoute",
     author_email="",
     url="https://github.com/gitmstoute/send_nsca3",
